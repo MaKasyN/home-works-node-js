85a337333c19ab78d6ef41312bfc5de6807749c8
kepka <kepka.max.ua@ukr.net>
2020-03-09 17:34:10 +0200

    send methods instead object

--- hw1/task #2/main.js
+++ hw1/task #2/main.js
@@ -4,10 +4,10 @@ const testArray = [13234, 324, 4, 3432, 23, 4, 3, 2, 2];
 const testNegativeNumber = -4;
 const testPositiveNumber = 4;
 
-console.log(`min number of number array: ${math.minNumber.getMinNumber(testArray)}`);
-console.log(`summ of number array: ${math.summOfNumbers.getSumm(testArray)}`);
+console.log(`min number of number array: ${math.minNumber(testArray)}`);
+console.log(`summ of number array: ${math.summOfNumbers(testArray)}`);
 console.log(
-    `module of negative test number ${math.moduleOfNumber.getModule(testNegativeNumber)}; 
-    module of positive test number ${math.moduleOfNumber.getModule(testPositiveNumber)}; 
-    function does work: ${math.moduleOfNumber.getModule(testNegativeNumber) === math.moduleOfNumber.getModule(testPositiveNumber)}.`
+    `module of negative test number ${math.moduleOfNumber(testNegativeNumber)}; 
+    module of positive test number ${math.moduleOfNumber(testPositiveNumber)}; 
+    function does work: ${math.moduleOfNumber(testNegativeNumber) === math.moduleOfNumber(testPositiveNumber)}.`
 );
